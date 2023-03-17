a1eab57d6e84215c64779543baf1578fda73223b
Naman2206 <namanmehta226@gmail.com>
2023-03-17 23:28:28 +0530

    push

---
+++ js/client.js
@@ -0,0 +1,9 @@
+const socket =io('http://localhost:8000');
+
+const form =document.getElementById('send-container');
+const messageInput =document.getElementById('messageInp')
+const messagecontainer=document.querySelector('.container')
+
+
+const Name = prompt('Enter your name to join');
+socket.emit('new-user-joined', Name);
